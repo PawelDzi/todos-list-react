4ce223a85d3a2fdd6657c5589a5494384f16b7dd
Pawel Dzierzbicki <ativek23@gmail.com>
2022-12-30 22:02:48 +0100

    toggleHideDome interaction

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
@@ -10,12 +11,17 @@ const tasks = [
   { id: 2, content: "zjeść obiad", done: true },
 ];
 
-const hideDone = false;
 
 function App() {
+  const [hideDone, setHideDone] = useState(false);
+
+  const toggleHideDone = () => {
+    setHideDone(hideDone => !hideDone);
+  };
+
   return (
     <Container>
-      <Title title="Lista zadań"/>
+      <Title title="Lista zadań" />
 
       <Section
         title="Dodaj nowe zadanie"
@@ -28,7 +34,11 @@ function App() {
           <Tasks tasks={tasks} hideDone={hideDone} />
         }
         extraHeaderContent={
-          <Buttons tasks={tasks} hideDone={hideDone} />
+          <Buttons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+          />
         }
       />
 
