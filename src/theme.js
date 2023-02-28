e2c1767ae033b5b5772d011b9a1a19426776411a
Pawel Dzierzbicki <ativek23@gmail.com>
2023-02-28 21:00:56 +0100

    added themeStyle

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+export const theme = {
+    color: {
+        teal: "#008080",
+        silver: "#ccc",
+        bondiBlue: "#00B3B3",
+        white: "#fff",
+        pineGreen: "#007070",
+        alto: "#ddd",
+        japaneseLaurel: "#008000",
+        camarone: "#006000",
+        guardsmanRed: "#cc0202",
+        brightRed: "#AA0000",
+        Gallery: "#F0EFEF",
+    },
+    breakpoint: {
+        mobileMax:767,
+    },
+};
